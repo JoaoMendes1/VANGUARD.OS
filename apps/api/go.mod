6a74fb47214b43485b96608e6643021c9e2007ae
João Victor Mendes <50036673+JoaoMendes1@users.noreply.github.com>
2026-03-12 20:37:49 +0000

    feat: initialize go server and connect to postgres (closes #4)

---
+++ apps/api/go.mod
@@ -0,0 +1,5 @@
+module vanguard-api
+
+go 1.25.4
+
+require github.com/lib/pq v1.11.2 // indirect
---
+++ apps/api/main.go
@@ -0,0 +1,45 @@
+package main 
+
+import (
+		"database/sql"
+		"fmt"
+		"log"
+		"net/http"
+
+		_ "github.com/lib/pq" // Importa o driver do PostGres silenciosamente 
+)
+
+func main() {
+	// 1. Configuração da string de conexão com o banco
+	// Usamos a porta 5433 que configuramos no Docker para evitar o conflito do Codespaces!
+	connStr := "user=root password=root dbname=vanguard_os port=5433 sslmode=disable host=localhost"
+
+	// 2. Abrindo a conexão
+	db, err := sql.Open("postgres",  connStr)
+	if err != nil {
+		log.Fatal("Failed to open a DB connection: ", err)
+	}
+	defer db.Close()
+
+	// 3. Testando se o banco realmente responde (Ping)
+	err = db.Ping()
+	if err != nil {
+		log.Fatal("Failed to ping the database! Is Docker Running? Error: ", err)
+	}
+
+	fmt.Println("🚀 [VANGUARD.OS] SUCCESSFULLY CONNECTED TO PostgreSQL!")
+
+	// 4. Criando uma rota de teste (Health Check)
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("VANGUARD.OS Engine is online and ready."))
+	})
+
+	// 5. Subindo o servidor na porta 8080
+	fmt.Println("⚡ API Server running on http://localhost:8080")
+	err = http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal("Server crashed: ", err)
+	}
+}
+
